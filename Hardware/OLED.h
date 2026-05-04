9c2bc349c44701d046221171f517917b5d3ec6bd
smile238 <15078948431@163.com>
2026-05-04 16:45:24 +0800

    Add files via upload

---
+++ Hardware/OLED.h
@@ -0,0 +1,16 @@
+#ifndef __OLED_H
+#define __OLED_H
+
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_ShowChar(uint8_t Line, uint8_t Column, char Char);//显示字符
+void OLED_ShowString(uint8_t Line, uint8_t Column, char *String);//显示字符串
+void OLED_ShowNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);//显示无符号数字
+void OLED_ShowSignedNum(uint8_t Line, uint8_t Column, int32_t Number, uint8_t Length);//显示有符号的十进制数字
+void OLED_ShowHexNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);//显示十六进制数
+void OLED_ShowBinNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);//显示二进制数
+void OLED_ShowCHINESE(uint8_t Line, uint8_t Column, uint8_t Num);//显示汉字
+
+void OLED_WriteCommand(uint8_t Command);//OLED写命令
+
+#endif
